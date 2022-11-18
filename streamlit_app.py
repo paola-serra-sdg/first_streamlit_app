04d58838da6fe1193afd51e671f28c1427082eaf
Paola-Serra <116500006+paola-serra-sdg@users.noreply.github.com>
2022-11-18 16:38:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents new healthy diner')
-🥣 🥗 🐔 🥑🍞
+
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
