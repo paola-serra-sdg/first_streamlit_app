f02dfd9eda61ed0900e3abeabb331fbe3a11b19e
Paola-Serra <116500006+paola-serra-sdg@users.noreply.github.com>
2022-11-18 16:34:30 +0100

    Update streamlit_app.py
    update menu file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
-streamlit.title('prova')
+streamlit.title('My Parents new healthy diner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('Poke 1')
+streamlit.text('Poke 2')
+streamlit.text('Poke 3')
+
